63ad4c4cb53c2e64cae6bb2a7a6d26acf8e266a0
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-11-18 01:58:02 +0100

    Add basic schema definition.

---
+++ schema.sql
@@ -0,0 +1,158 @@
+-- Temporarily disable constraint checks to re-create the database.
+EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'
+EXEC sp_msforeachtable 'DROP TABLE ?'
+EXEC sp_msforeachtable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all'
+
+/*
+ * id
+ * name - conference name
+ * city, street, postal_code, building_number - address details
+ * student_discount - discount for students in range [0.0000, 1.0000]
+ **/
+CREATE TABLE conferences (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  street VARCHAR(50) NOT NULL,
+  postal_code VARCHAR(10) NOT NULL,
+  building_number VARCHAR(10) NOT NULL,
+  student_discount DECIMAL(5, 4) NOT NULL DEFAULT 0
+);
+
+/*
+ * id
+ * conference_id
+ * date - date of the day
+ * attendee_limit - maximal number of attendees for the day
+ **/
+CREATE TABLE conference_days (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  conference_id INT NOT NULL FOREIGN KEY REFERENCES conferences(id),
+  date DATE NOT NULL,
+  attendee_limit INT NOT NULL
+);
+
+/*
+ * id
+ * conference_day_id
+ * name, description - workshop information
+ * start, end - workshop time range
+ * room - information about the location within the conference building
+ * price - fixed workshop price
+ * attendee_limit - maximal number of attendees for the workshop
+ **/
+CREATE TABLE workshops (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  conference_day_id INT NOT NULL FOREIGN KEY REFERENCES conference_days(id),
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
+  start_time TIME NOT NULL,
+  end_time TIME NOT NULL,
+  room VARCHAR(30) NOT NULL,
+  price MONEY NOT NULL,
+  attendee_limit INT NOT NULL
+);
+
+/*
+ * id
+ * conference_id
+ * final_date - date until which the given price applies
+ * price - price that applies until the final_date
+ **/
+CREATE TABLE conference_prices (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  conference_id INT NOT NULL FOREIGN KEY REFERENCES conferences(id),
+  final_date DATE NOT NULL,
+  price MONEY NOT NULL
+);
+
+/*
+ * id
+ **/
+CREATE TABLE clients (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  city VARCHAR(50) NOT NULL,
+  street VARCHAR(50) NOT NULL,
+  postal_code VARCHAR(10) NOT NULL,
+  building_number VARCHAR(10) NOT NULL
+);
+
+/*
+ * id
+ **/
+CREATE TABLE companies (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  client_id INT NOT NULL FOREIGN KEY REFERENCES clients(id),
+  name VARCHAR(50) NOT NULL,
+  phone VARCHAR(15) NOT NULL
+);
+
+/*
+ * id
+ **/
+CREATE TABLE people (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  phone VARCHAR(15) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  student_card_id VARCHAR(20)
+);
+
+/*
+ * id
+ **/
+CREATE TABLE individual_clients (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  client_id INT NOT NULL FOREIGN KEY REFERENCES clients(id),
+  person_id INT NOT NULL FOREIGN KEY REFERENCES people(id)
+);
+
+/*
+ * id
+ **/
+CREATE TABLE bookings (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  client_id INT NOT NULL FOREIGN KEY REFERENCES clients(id),
+  created_at DATETIME NOT NULL
+);
+
+/*
+ * id
+ **/
+CREATE TABLE day_bookings (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  booking_id INT NOT NULL FOREIGN KEY REFERENCES bookings(id),
+  conference_day_id INT NOT NULL FOREIGN KEY REFERENCES conference_days(id),
+  attendee_count INT NOT NULL,
+  student_count INT NOT NULL
+);
+
+/*
+ * id
+ **/
+CREATE TABLE workshop_bookings (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  day_booking_id INT NOT NULL FOREIGN KEY REFERENCES day_bookings(id),
+  workshop_id INT NOT NULL FOREIGN KEY REFERENCES workshops(id),
+  attendee_count INT NOT NULL,
+  student_count INT NOT NULL
+);
+
+/*
+ * id
+ **/
+CREATE TABLE day_enrollments (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  day_booking_id INT NOT NULL FOREIGN KEY REFERENCES day_bookings(id),
+  attendee_id INT NOT NULL FOREIGN KEY REFERENCES people(id)
+);
+
+/*
+ * id
+ **/
+CREATE TABLE workshop_enrollments (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  day_enrollment_id INT NOT NULL FOREIGN KEY REFERENCES day_enrollments(id),
+  workshop_booking_id INT NOT NULL FOREIGN KEY REFERENCES workshop_bookings(id)
+);
