3654a2329f19b12f62084e3238a4547397843e61
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-01-11 18:31:15 +0100

    Add procedure for adding a conference price.

--- src/procedures.sql
+++ src/procedures.sql
@@ -62,3 +62,26 @@ BEGIN CATCH
   DECLARE @error NVARCHAR(2048) = 'Failed to add the workshop. Got an error: ' + ERROR_MESSAGE();
   THROW 51000, @error, 1;
 END CATCH;
+
+DROP PROCEDURE IF EXISTS add_conference_price;
+CREATE PROCEDURE add_conference_price
+  @conference_id INT,
+  @final_date DATE,
+  @price MONEY
+AS
+BEGIN TRY
+  IF NOT EXISTS (SELECT 1 FROM conferences WHERE id = @conference_id)
+  BEGIN
+    THROW 51000, 'Conference with the given id does not exist.', 1;
+  END
+  IF EXISTS (SELECT 1 FROM conference_days WHERE conference_id = @conference_id AND @final_date = @final_date)
+  BEGIN
+    THROW 51000, 'Conference price with the given final date already exists.', 1;
+  END
+  INSERT INTO conference_prices (conference_id, final_date, price)
+  VALUES (@conference_id, @final_date, @price);
+END TRY
+BEGIN CATCH
+  DECLARE @error NVARCHAR(2048) = 'Failed to add the conference price. Got an error: ' + ERROR_MESSAGE();
+  THROW 51000, @error, 1;
+END CATCH;
